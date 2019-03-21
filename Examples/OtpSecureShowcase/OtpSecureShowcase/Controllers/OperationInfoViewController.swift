fd1b0a2012836148702c113d4e24118be1a1c0db
Diego Sepúlveda Blanco <d.sepulvedabl@gmail.com>
2019-03-21 18:39:46 +0100

    Add sample app

---
+++ Examples/OtpSecureShowcase/OtpSecureShowcase/Controllers/OperationInfoViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  OtpSecureShowcase
+//
+//  Created by Diego Sepúlveda Blanco on 07/03/2019.
+//  Copyright © 2019 Ecertic. All rights reserved.
+//
+
+import UIKit
+
+import OtpSecure
+
+class OperationInfoViewController: UIViewController {
+    
+    let FROM_OPERATION_INFO_TO_VALIDATE_TOKEN_VC = "toValidateTokenViewController"
+    
+    var token: String = ""
+    
+    @IBOutlet weak var tokenTextField: UITextField!
+    
+    @IBOutlet weak var mandatoryTokenLabel: UILabel!
+    
+    @IBOutlet weak var invalidTokenLabel: UILabel!
+    
+    @IBAction func onRetrieveOperationInfo(_ sender: Any) {
+        mandatoryTokenLabel.isHidden = true
+        invalidTokenLabel.isHidden = true
+        
+        token = tokenTextField.text!.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+        if token.isEmpty {
+            mandatoryTokenLabel.isHidden = false
+            return;
+        }
+        
+        OtpSecure.retrieveOperationInfoByToken(token: token, completionHandler: onOperationInfoRetrieved)
+
+    }
+    
+    func onOperationInfoRetrieved(result: Result<OperationInfo>) -> Void {
+        switch result {
+        case .success(let data):
+            if data.status != nil {
+                self.setErrorText(text: data.html)
+                return
+            }
+            print("Success --> prepare for segue with data: \(data)")
+            DispatchQueue.main.async(execute: {
+                self.performSegue(withIdentifier: self.FROM_OPERATION_INFO_TO_VALIDATE_TOKEN_VC, sender: data)
+            })
+        case .failure(let error):
+            print("Error while retrieving token --> \(error)")
+            self.setErrorText(text: error.localizedDescription)
+        }
+    }
+
+    func setErrorText(text: String) {
+        DispatchQueue.main.async(execute: {
+            self.invalidTokenLabel.text = text
+            self.invalidTokenLabel.isHidden = false
+        })
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if identifier == FROM_OPERATION_INFO_TO_VALIDATE_TOKEN_VC {
+                if let validateTokenViewController = segue.destination as? ValidateTokenViewController {
+                    validateTokenViewController.token = token
+                    validateTokenViewController.operationInfo = (sender as! OperationInfo)
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
---
+++ Examples/OtpSecureShowcase/OtpSecureShowcase/Controllers/ValidateTokenViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ValidateViewController.swift
+//  OtpSecureShowcase
+//
+//  Created by Diego Sepúlveda Blanco on 14/03/2019.
+//  Copyright © 2019 Ecertic. All rights reserved.
+//
+
+import UIKit
+
+import OtpSecure
+
+class ValidateTokenViewController: UIViewController {
+    
+    var token: String!
+    
+    var operationInfo: OperationInfo!
+    
+    @IBOutlet weak var htmlLabel: UILabel!
+    
+    @IBOutlet weak var otpTextField: UITextField!
+    
+    @IBOutlet weak var mandatoryOtpLabel: UILabel!
+    
+    @IBOutlet weak var invalidOtpLabel: UILabel!
+    
+    @IBAction func onValidateOtp(_ sender: Any) {
+        mandatoryOtpLabel.isHidden = true
+        invalidOtpLabel.isHidden = true
+
+        let otp = otpTextField.text!.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+        if otp.isEmpty {
+            mandatoryOtpLabel.isHidden = false
+            return;
+        }
+
+        OtpSecure.validateToken(token: token, otp: otp, completionHandler: onTokenValidated)
+
+    }
+    
+    func onTokenValidated(result: Result<Validation>) -> Void {        
+        switch result {
+        case .success(let data):
+            print("Success --> data: \(data)")
+            self.setErrorText(text: data.msg)
+        case .failure(let error):
+            print("Error while retrieving token --> \(error)")
+            self.setErrorText(text: error.localizedDescription)
+        }
+    }
+    
+    func setErrorText(text: String) {
+        DispatchQueue.main.async(execute: {
+            self.invalidOtpLabel.text = text
+            self.invalidOtpLabel.isHidden = false
+        })
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("ValidateViewController.token --> \(token!)")
+        print("ValidateViewController.operationInfo --> \(operationInfo!)")
+        
+        htmlLabel.text = operationInfo.html
+    }
+}
